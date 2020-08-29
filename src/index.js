ecdc255cff493127b92c9c6e50c9740d2f1928dc
Lucas Duarte <l.duarte.mk@gmail.com>
2020-08-29 13:24:51 -0300

    css added

--- src/index.js
+++ src/index.js
@@ -3,31 +3,33 @@ import api from './services/api';
 const component = document.getElementById('cep-component');
 
 component.innerHTML = `
+<h3> Consulta CEP</h3>
   <form id="cep-form">
     <div class="form-field">
-      <label for="cep">cep:</label>
+    <div id="header">
       <input type="text" id="cep" placeholder="Digite seu cep" required>
+      <button id="busca">Buscar</button>
     </div>
-    <button id="busca">Consultar CEP</button>
+  </div>
   </form>
-  <div id="dadosCep"></div>
+  <div class="dadosCep hidden"></div>
 `;
 
 const inputElement = component.querySelector('#cep');
 const buttonElement = component.querySelector('#busca');
-const dadosCep = component.querySelector('#dadosCep');
+const dadosCep = component.querySelector('.dadosCep');
 
 buttonElement.onclick = function findCep(event) {
   event.preventDefault();
 
   const inputText = inputElement.value;
+  dadosCep.classList.remove('hidden')
 
   api.get(`/v1/cep/${inputText}`).then(response => {
     const { data } = response;
-
     
     dadosCep.innerHTML = `
-      <p>Logradouro: ${data.logradouro}</p>
+      <p class="first">Logradouro: ${data.logradouro}</p>
       <p>Bairro: ${data.bairro}</p>
       <p>Cidade: ${data.cidade}</p>
       <p>Estado: ${data.estado}</p>
@@ -35,7 +37,7 @@ buttonElement.onclick = function findCep(event) {
     `
   }).catch(() => {
     dadosCep.innerHTML = `
-    <p class="error">O CEP: ${inputText} é inválido!</p>
+    <p class="error first">O CEP: ${inputText} é inválido!</p>
   `
   });
 }
